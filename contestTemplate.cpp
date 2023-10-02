94b3def65398ce5626d3e5ba9dfdfda919389522
ftanvir <ft.tanvir551@gmail.com>
2023-10-02 20:18:06 +0600

    added new one

---
+++ contestTemplate.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+
+
+#define all(a)         a.begin(),a.end()
+#define Sort(x)        sort(x.begin(),x.end())
+#define SortR(x)       sort(x.begin(),x.end(),greater<int>())
+#define Reverse(x)     reverse(x.begin(),x.end())
+
+#define SortA(ar,s)    sort(ar,ar+s)
+#define SortD(ar,s)    sort(ar,ar+s,greater<int>())
+
+#define gcd(a,b)       __gcd(a,b)
+#define lcm(a,b)       (a*(b/gcd(a,b)))
+#define sq(x)          (x)*(x)
+#define mid(l,r)       (l+(r-l)/2)
+#define precision(x)      cout<<fixed<<setprecision(x)
+#define yes               cout<<"YES"<<nl
+#define no                cout<<"NO"<<nl
+#define min_heap priority_queue<ll,vector<ll>,greater<ll>>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+#define testcase ll t;cin>>t;while(t--)
+
+/*
+sort vector of pair comparator lambda function
+vector<pair<ll, ll>> vpi;
+sort(all(vpi), [&](pair<ll, ll>x, pair<ll,ll>y){
+        if(x.ff==y.ff){
+            return (x.ss<y.ss);
+        }else{
+            return (x.ff>y.ff);
+        }
+});
+*/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{[first]: "; _print(p.ff); cerr << ",[second]: "; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+template <class T>
+void print_array(const T arr[], int size) {
+    cerr << "[ ";
+    for (int i = 0; i < size; ++i) {
+        _print(arr[i]);
+        cerr << " ";
+    }
+    cerr << "]"<<nl;
+}
+int tc = 1;
+
+
+
+void cholokori()
+{
+    
+    
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("debug.txt", "w", stderr);
+#endif
+
+testcase{
+    // cout<<"Case "<<tc++<<": ";
+    cholokori();
+}
+
+return 0;
+
+}
